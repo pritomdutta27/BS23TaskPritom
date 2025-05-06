f3c44afc1ee8cc588238308be68a7c8cf747e4ee
Pritom Dutta <pritom00019@gmail.com>
2025-05-06 17:55:55 +0600

    paging data added

--- app/src/main/java/com/pritomdutta/bs23taskpritom/ui/fragment/HomeViewModel.kt
+++ app/src/main/java/com/pritomdutta/bs23taskpritom/ui/fragment/HomeViewModel.kt
@@ -22,15 +22,6 @@ class HomeViewModel @Inject constructor(private val repository: StockRepository)
 
     var _pagingDataFlow: LiveData<PagingData<ShowDisplayStockData>>? = null
 
-//    val pagingDataFlow = Pager(
-//        config = PagingConfig(pageSize = 10, maxSize = 200),
-//        pagingSourceFactory = {
-//            StockPagingSource(onRes = { page ->
-//                data(page) // Now it returns ShowDisplayStockResponse
-//            })
-//        }
-//    ).flow.cachedIn(viewModelScope)
-
     fun fetchStockData() {
         _pagingDataFlow = Pager(
             config = PagingConfig(pageSize = 10, maxSize = 200),
@@ -39,9 +30,4 @@ class HomeViewModel @Inject constructor(private val repository: StockRepository)
             }
         ).liveData
     }
-
-
-//    suspend fun data(page: Int){
-//        repository.fetchStockData(page)
-//    }
 }
